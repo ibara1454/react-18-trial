849322102dd5a91ea69866463bc58c9856f7b80a
Chiajun Wang <ibara1454@gmail.com>
2022-06-17 04:09:18 +0900

    added menu / router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import ShopItem from "./ShopItem/Static";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SuspenseExampleStatic from "./SuspenseExample/Static";
+import SuspenseExampleStandard from "./SuspenseExample/Standard";
+import SuspenseExampleSuspense from "./SuspenseExample/Suspense";
+import SideMenu from "./SideMenu";
 
 function App() {
   return (
-    <div className="min-h-screen bg-zinc-900 flex items-center justify-center">
-      <ShopItem />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-zinc-900 flex">
+        <SideMenu />
+        <main className="flex-auto flex justify-center items-center">
+          <Routes>
+            <Route path="/" element={<SuspenseExampleStatic />} />
+            <Route
+              path="/suspense_example/"
+              element={<SuspenseExampleStatic />}
+            />
+            <Route
+              path="/suspense_example/standard"
+              element={<SuspenseExampleStandard />}
+            />
+            <Route
+              path="/suspense_example/suspense"
+              element={<SuspenseExampleSuspense />}
+            />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/SideMenu/index.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+
+const SideMenu = () => {
+  return (
+    <aside className="w-96 bg-slate-100 p-4 overflow-auto">
+      <p className="text-gray-400 text-lg font-bold uppercase">
+        Suspense Data Fetching Example
+      </p>
+      <ul className="m-8 flex flex-col gap-8">
+        <li>
+          <NavLink to="/suspense_example">Static</NavLink>
+        </li>
+        <li>
+          <NavLink to="/suspense_example/standard">Standard</NavLink>
+        </li>
+        <li>
+          <NavLink to="/suspense_example/suspense">Suspense</NavLink>
+        </li>
+      </ul>
+      <p className="text-gray-400 text-lg font-bold uppercase">
+        Transition Example
+      </p>
+      <ul className="m-8 flex flex-col gap-8">
+        <li>
+          <NavLink to="/transition_example">Example</NavLink>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default SideMenu;
